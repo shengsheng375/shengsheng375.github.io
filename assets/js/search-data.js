2a1d3a0bd74fc65f86335ca3c308e336ff326ff8
edisons3608 <57467707+edisons3608@users.noreply.github.com>
2025-06-17 16:38:20 +0000

    Deploying to gh-pages from @ shengsheng375/shengsheng375.github.io@13cb6f838677d6864d8d0ce7d5063c94f68e4dc1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,12 +30,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather.html";
-            },},{id: "projects-project-1",
+        },{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
